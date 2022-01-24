5673ce675ca8aaef0e1ac4355b4dd8d54e05247f
siwook <jsu0638@naver.com>
2022-01-25 00:19:33 +0900

    Update 로또의_최고_순위와_최저_순위.py

--- siwook/로또의_최고_순위와_최저_순위.py
+++ siwook/로또의_최고_순위와_최저_순위.py
@@ -22,3 +22,19 @@ def solution(lottos, win_nums):
         answer.append(rank)
     
     return answer
+
+
+"""
+in 사용과 return 숫자, 숫자 => [숫자, 숫자
+
+def solution(lottos, win_nums):
+
+    rank=[6,6,5,4,3,2,1]
+
+    cnt_0 = lottos.count(0)
+    ans = 0
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
+    return rank[cnt_0 + ans],rank[ans]
+"""
