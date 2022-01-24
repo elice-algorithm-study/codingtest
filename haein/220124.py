82d69ac878d3613bdd8005e9a188d625d1f44b8f
Haein-bite <75101582+Haein-bite@users.noreply.github.com>
2022-01-24 22:36:44 +0900

    lv1. 신고결과 받기

--- haein/220124.py
+++ haein/220124.py
@@ -1,1 +1,14 @@
-print('hi')
+def solution(id_list, report, k): 
+    answer = [0] * len(id_list)
+    dic = {id:[] for id in id_list}
+    
+    for re in set(report): # 중복해제
+        list = re.split()
+        dic[list[1]].append(list[0])
+
+    for key, val in dic.items(): # 키와 값들의 쌍을 하나씩 얻음
+        if len(val) >= k:
+            for v in val:
+                answer[id_list.index(v)] += 1
+        
+    return answer
