d28530387561956833eb90b377ae11b2d88305ce
siwook <jsu0638@naver.com>
2022-01-27 21:47:17 +0900

    100

--- siwook/신규_아이디_추천.py
+++ siwook/신규_아이디_추천.py
@@ -23,7 +23,6 @@ def solution(new_id):
         else:
             break
 
-
     # 5 단계
     if len(new_id) == 0:
         new_id += "a"
@@ -41,4 +40,21 @@ def solution(new_id):
         new_id += new_id[-1]
 
     answer += new_id
+    
     return answer
+
+
+"""
+import re
+
+def solution(new_id):
+    st = new_id
+    st = st.lower()
+    st = re.sub('[^a-z0-9\-_.]', '', st)
+    st = re.sub('\.+', '.', st)
+    st = re.sub('^[.]|[.]$', '', st)
+    st = 'a' if len(st) == 0 else st[:15]
+    st = re.sub('^[.]|[.]$', '', st)
+    st = st if len(st) > 2 else st + "".join([st[-1] for i in range(3-len(st))])
+    return st
+"""
