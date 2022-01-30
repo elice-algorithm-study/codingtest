049ebc577f0175d7ba6db990cc138ce580b25389
kminzy <kminzy0704@gmail.com>
2022-01-30 23:24:47 +0900

    220130 평균 구하기, 콜라츠 추측

---
+++ minzy/콜라츠 추측.py
@@ -0,0 +1,16 @@
+def solution(num):
+    answer = 0
+
+    while num != 1:
+        if answer == 500:
+            answer = -1
+            break
+        else:
+            if num % 2 == 0:
+                num /= 2
+                answer += 1
+            else:
+                num = num * 3 + 1
+                answer += 1
+
+    return answer
