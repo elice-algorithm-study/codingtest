6d6c586c831312c244e5f3babcb6d7f0e55516cb
진종 <56257923+skyfjinjong@users.noreply.github.com>
2022-01-31 22:27:09 +0900

    220130-02 콜라츠 추측 문제풀이
    220130-02 콜라츠 추측 문제풀이(python)입니다.

---
+++ jinjong/220130-02.py
@@ -0,0 +1,13 @@
+def solution(num):
+    answer = 0
+    while num != 1:
+        if answer==500:
+            answer = -1
+            break
+        if num%2==0:
+            num /= 2
+            answer+=1
+        else:
+            num = num*3 + 1
+            answer+=1
+    return answer
