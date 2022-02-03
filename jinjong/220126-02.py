bee3c9aaa8b808c3fdf8183fa8fc900a5082ba5e
진종 <56257923+skyfjinjong@users.noreply.github.com>
2022-02-03 23:42:25 +0900

    220126-02 숫자 문자열과 영단어 문제풀이
    220126-02 숫자 문자열과 영단어 문제풀이(python)입니다.

--- jinjong/220126-02.py
+++ jinjong/220126-02.py
@@ -1,11 +1,20 @@
 def solution(s):
-  eng = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-  answer = ''
+    numDict = {'zero': 0, 'one' : 1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6,
+              'seven':7, 'eight':8, 'nine':9}
+    for value, key in numDict.items():
+        if value in s:
+            s = s.replace(value, str(key))
+    answer = int(s)
+    return answer
   
-  for i, num in enumerate(eng):
+# def solution(s):
+#   eng = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+#   answer = ''
+  
+#   for i, num in enumerate(eng):
     
-    if num in s:
-      s = s.replace(num, str(i))
-    answer = s
+#     if num in s:
+#       s = s.replace(num, str(i))
+#     answer = s
     
-  return int(answer)
+#   return int(answer)
