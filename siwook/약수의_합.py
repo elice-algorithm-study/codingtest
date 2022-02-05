d5c424e6f731f9cefa7089e6406f0125e9706e3d
siwook <jsu0638@naver.com>
2022-02-05 18:42:56 +0900

    100

--- siwook/약수의_합.py
+++ siwook/약수의_합.py
@@ -11,5 +11,5 @@ def solution(n):
 """
 def sumDivisor(num):
     # num / 2 의 수들만 검사하면 성능 약 2배 향상잼
-    return num + sum([i for i in range(1, (num * 0.5) + 1) if num % i == 0])
+    return num + sum([i for i in range(1, (num // 2) + 1) if num % i == 0])
 """
