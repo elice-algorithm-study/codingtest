c7dac33a33a93cf5292202f41869d1a58b2d4ab2
siwook <jsu0638@naver.com>
2022-02-05 18:33:16 +0900

    100

---
+++ siwook/서울에서_김서방_찾기.py
@@ -0,0 +1,13 @@
+def solution(seoul):
+    answer = ""
+    i = str(seoul.index("Kim"))
+    answer += "김서방은 "
+    answer += i
+    answer += "에 있다"
+    return answer
+  
+
+"""
+def findKim(seoul):
+    return "김서방은 {}에 있다".format(seoul.index('Kim'))
+"""
