0db905ca61d17c973a8dd432ef445d20031cf021
진종 <56257923+skyfjinjong@users.noreply.github.com>
2022-02-15 00:19:02 +0900

    Update 220214-01.py
    모의고사 풀이코드 수정

--- jinjong/220214-01.py
+++ jinjong/220214-01.py
@@ -2,50 +2,46 @@ from collections import deque
 
 def solution(answers):
     answer = []
-    score = {
-        'ans1' : 0,
-        'ans2' : 0,
-        'ans3' : 0
-    }
+    score = []
+    ans1 = ans2 = ans3 = 0
     
     idx = 1
     for i in range(len(answers)):
         if answers[i] == idx:
-            score['ans1'] += 1        
+            ans1 += 1        
         if idx < 5:
             idx += 1
         else:
             idx = 1
+    score.append((1, ans1))
     
-    dq = deque([1, 3, 4, 5])
+    dq2 = deque([1, 3, 4, 5])
     for i in range(len(answers)):
-        if i%2==0 and answers[i] == 2:
-            score['ans2'] += 1
-        elif i%2!=0:
-            if answers[i] == dq[0]:
-                score['ans2'] += 1
-            dq.rotate(-1)
+        if i%2==0:
+            if answers[i] == 2:
+                ans2 += 1
+            continue
+
+        elif i%2!=0 and answers[i] == dq2[0]:
+            ans2 += 1
+        dq2.rotate(-1)
+    score.append((2, ans2))
     
     dq3 = deque([3, 1, 2, 4, 5])
     for i in range(len(answers)):
-        if i%2==0 and answers[i] == dq3[0]:
-            score['ans3'] += 1
-        elif i%2!=0:
+        if i%2==0:
             if answers[i] == dq3[0]:
-                score['ans3'] +=1
-            dq3.rotate(-1)
+                ans3 += 1
+            continue
+            
+        elif i%2!=0 and answers[i] == dq3[0]:
+            ans3 +=1
+        dq3.rotate(-1)
+    score.append((3, ans3))
     
-    score = sorted(score.items(), key=(lambda x: x[1]), reverse = True)
-    
-    for i in range(len(score) - 1):
-        if score[i][1] == score[i+1][1]:
-            answer.append(i+1)
-            answer.append(i+2)
-        else:
-            answer.append(i+1)
-            break
+    maxNum = max(ans1, ans2, ans3)
+    for i in range(len(score)):
+        if score[i][1] == maxNum:
+            answer.append(score[i][0])
         
-    tmp = set(answer)
-    answer = list(tmp)
-    
     return answer
