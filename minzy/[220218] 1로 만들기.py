d52ac56ea6b20906e5004e866beffcf40fc712b8
kminzy <kminzy0704@gmail.com>
2022-02-18 18:39:12 +0900

    220217

---
+++ minzy/[220218] 1로 만들기.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+d = [0] * (n + 1) # 배열 초기화
+
+d[1] = 0 # 1에서 1로 가는 경우는 0가지로 설정
+
+for i in range(2, n + 1):
+    d[i] = d[i - 1] + 1
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i // 3] + 1)
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i // 2] + 1)
+
+print(d[n])
